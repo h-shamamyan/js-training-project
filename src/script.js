de7a6f689073115028457159f301629dbfa19b72
Hovsep Shamamyan <hshamamyan@servicetitan.com>
2025-01-28 19:37:26 +0400

    deleted files

--- src/script.js
+++
@@ -1,4 +0,0 @@
-document.body.innerHTML += '<button id="alertButton">Click Me!</button>';
-document.getElementById('alertButton').addEventListener('click', () => {
-    alert('Button clicked!');
-});
